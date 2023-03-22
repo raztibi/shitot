2acb257580694ed866b54dfc1d6c208fb37a08fd
nchmoka <87582118+nchmoka@users.noreply.github.com>
2023-03-22 10:20:58 +0200

    Update and rename zona.java to test.java
    matan

---
+++ src/test.java
@@ -0,0 +1,8 @@
+
+public class test {
+	double number =1.5;
+	public String toString()
+	{
+		return "nice test " + zin + "works";
+	}
+}
--- src/zona.java
+++
@@ -1,8 +0,0 @@
-
-public class zona {
-	double zin =1.5;
-	public String toString()
-	{
-		return "nice pleasure " + zin + "inchis";
-	}
-}
